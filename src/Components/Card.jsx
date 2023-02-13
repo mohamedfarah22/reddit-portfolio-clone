5a338c4b8bcf0ea7077a46518354a29273109bdb
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-13 17:32:23 +1100

    feat: added name of subreddit and image to design styling guidelines

---
+++ src/Components/Card.jsx
@@ -0,0 +1,10 @@
+import fitnessLogo from '../../src/r:fitness.png'
+export function Card(){
+
+    return (
+        <div>
+            <img src= {fitnessLogo}/>
+            <h1>r/fitness</h1>
+        </div>
+    )
+}
