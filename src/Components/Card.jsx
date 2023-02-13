772e5efbeada6c31c09b95d747b3c7de19c6c38c
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-13 18:37:16 +1100

    feat: added example subreddit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,12 @@
-import fitnessLogo from '../../src/r:fitness.png'
+import './Card.css';
 export function Card(){
 
     return (
         <div>
-            <img src= {'https://a.thumbs.redditmedia.com/IrfPJGuWzi_ewrDTBlnULeZsJYGz81hsSQoQJyw6LD8.png'}/>
-            <h1>r/askReddit</h1>
+        <div className='card-container'>
+            <img className="header-img" alt="header of subreddit" src= {'https://a.thumbs.redditmedia.com/IrfPJGuWzi_ewrDTBlnULeZsJYGz81hsSQoQJyw6LD8.png'}/>
+            <h5 className='display-name-prefixed'>r/askReddit</h5>
+        </div>
         </div>
     )
 }
