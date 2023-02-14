84f1f5334bf060071e13a03b3f954dc6134c8fb8
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-14 13:09:06 +1100

    fix: fixed dispatch call to load posts by adding brackets at end of function

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector} from "react-redux";
 import { selectPosts } from "../../store/redditSlice";
-import {loadPosts} from '../../store/redditSlice'
+import {loadPosts} from '../../store/redditSlice';
+import { Posts } from "../../Components/postCards/Posts";
 export function Home(){
     const posts = useSelector(selectPosts);
     const dispatch = useDispatch();
 
     useEffect(() =>{
-        dispatch(loadPosts);
+        dispatch(loadPosts());
     }, [dispatch])
 return(
-    <Posts />
+    <Posts  posts = {posts}/>
 )
 }
