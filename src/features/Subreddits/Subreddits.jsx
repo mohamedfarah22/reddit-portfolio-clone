75906c7b2875835a96cf35eb4a6eb7d0f8602e5f
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-14 11:51:22 +1100

    changed state of the subreddit slice and passed down properties to presentational card component to display subreddits

--- src/features/Subreddits/Subreddits.jsx
+++ src/features/Subreddits/Subreddits.jsx
@@ -1,8 +1,28 @@
-import { Card } from "../../Components/Card"
+import { Card } from "../../Components/Card";                                                                                                                                                                                                                                                                                                                               
+import {useDispatch, useSelector} from 'react-redux';
+import { useEffect } from "react";
+import { loadSubreddits } from "../../store/subredditSlice";
+import { selectSubreddits } from "../../store/subredditSlice";
+import logo from '../../logo.png';
 export function Subreddits (){
+const dispatch = useDispatch();
+const subreddits = useSelector(selectSubreddits);
+
+  useEffect(() =>{
+    dispatch(loadSubreddits())
+  }, [dispatch])
 
     return(
-        <Card />
-
+      <div>
+        {subreddits.map(subreddit => {
+          if(subreddit['header_img'] !== null){
+          return <Card headerImg = {subreddit['header_img']} subredditName = {subreddit['display_name']}/>
+          }
+          else{
+            return <Card headerImg = {logo} subredditName = {subreddit['display_name']}/>
+          }
+        })}
+      </div>
+      
     )
 }
