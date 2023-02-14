bcda29c12e51843ed1e363ed06e3a586d6bae89e
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-14 12:51:08 +1100

    feat: added redditSlice reducer to be accessed by Home feature

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import subredditSliceReducer from './subredditSlice';
+import redditSliceReducer from './redditSlice';
 
 export const store = configureStore({
   reducer: {
     subRedditSlice: subredditSliceReducer,
+    redditSlice: redditSliceReducer
   },
 });
