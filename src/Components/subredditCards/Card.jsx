1ebc70ff41c370f50ee1924398d94409fdfab148
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-14 13:00:28 +1100

    fix: created file for card components

---
+++ src/Components/subredditCards/Card.jsx
@@ -0,0 +1,27 @@
+import './Card.css';
+import {useSelector} from 'react-redux';
+import { selectSubreddits } from '../../store/subredditSlice';
+export function Card(props){
+    const subreddits = useSelector(selectSubreddits)
+
+    return (
+
+       
+        <div className="subreddits-card-container">
+                    <div className='card-container'>
+                        <img className="header-img" alt="header of subreddit" src= {props.headerImg}/>
+                        <h5 className='display-name-prefixed'>{`r/${props.subredditName}`}</h5>
+                    </div> 
+        </div>            
+               
+
+            
+            
+
+        
+    )
+}
+/*<div className='card-container'>
+                    <img className="header-img" alt="header of subreddit" src= {subreddit['header_img']}/>
+                    <h5 className='display-name-prefixed'>{subreddit['display_name_prefix']}</h5>
+                </div> */
