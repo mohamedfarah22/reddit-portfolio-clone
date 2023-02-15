51ae7451f092c76b8b48e6583b20c0a80585ff12
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-16 09:18:47 +1100

    feat: imported selectSubreddit action handler and created click handler to dispatch action

--- src/Components/subredditCards/Card.jsx
+++ src/Components/subredditCards/Card.jsx
@@ -1,17 +1,21 @@
 import './Card.css';
-import {useSelector} from 'react-redux';
-import { selectSubreddits } from '../../store/subredditSlice';
+import {useSelector, useDispatch} from 'react-redux';
+import { setCurrentSubreddit } from '../../store/redditSlice';
 import { useState } from 'react';
-export function Card(props){
-    const [subreddit, setSubreddit] = useState('');
 
+
+export function Card(props){
+    const dispatch = useDispatch()
     
+    const onClickHandler = (e) =>{
+        dispatch(setCurrentSubreddit(`/r/${props.subredditName}`))
+    }
+
    
 
     return (
-
-       
-        <div className="subreddits-card-container">
+ 
+        <div onClick = {onClickHandler} className="subreddits-card-container">
                     <div className='card-container'>
                         <img className="header-img" alt="header of subreddit" src= {props.headerImg}/>
                         <h5 className='display-name-prefixed'>{`r/${props.subredditName}`}</h5>
