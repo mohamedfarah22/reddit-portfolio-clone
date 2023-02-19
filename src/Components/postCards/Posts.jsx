faee28427adfeeb24f9820928e8afac46999a5b8
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-19 20:46:13 +1100

    feat: added logic around formatting posts with videos/images

--- src/Components/postCards/Posts.jsx
+++ src/Components/postCards/Posts.jsx
@@ -25,6 +25,47 @@ const onMouseOutHandlerDown = (e) =>{
         <div className="feed-container">
             {
         props.posts.map((post) =>{
+        
+        if(post.thumbnail !== 'self' && post['is_video'] === false && post.thumbnail !== 'nsfw'){
+            return(
+                <div className = 'post-card-container'>
+                <div className= "vote-container">
+                        <button onMouseOver = {onMouseOverHandlerUpVote} onMouseOut = {onMouseOutHandlerUpVote} className='up-vote'><img onMouseOver = {onMouseOverHandlerUpVote} onMouseOut = {onMouseOutHandlerUpVote} src='https://styles.redditmedia.com/t5_2qnty/styles/postUpvoteIconInactive_n5ydt0uuj6x11.png' alt="up-vote"/></button>
+                        <p className = 'post-score'>{(post.ups)-(post.downs)}</p>
+                        <button  onMouseOver = {onMouseOverHandlerDown} onMouseOut = {onMouseOutHandlerDown} className ='down-vote'><img src='https://styles.redditmedia.com/t5_2qnty/styles/postDownvoteIconInactive_cnbj1c0wj6x11.png' alt ='down-vote'/></button>
+                </div>
+                <div className='post-container'>
+                    <h1 className='post-title'>{post.title}</h1>
+                    <p className = 'post-text'>{post.selftext}</p>
+                <div className = 'image-container'>
+                    <img className="post-image" src={post.thumbnail} alt="post"  />
+                </div>
+                </div>
+            </div>  
+            )
+        }
+      if(post.is_video === true){
+        return(
+       <div className = 'post-card-container'>
+            <div className= "vote-container">
+                    <button onMouseOver = {onMouseOverHandlerUpVote} onMouseOut = {onMouseOutHandlerUpVote} className='up-vote'><img onMouseOver = {onMouseOverHandlerUpVote} onMouseOut = {onMouseOutHandlerUpVote} src='https://styles.redditmedia.com/t5_2qnty/styles/postUpvoteIconInactive_n5ydt0uuj6x11.png' alt="up-vote"/></button>
+                    <p className = 'post-score'>{(post.ups)-(post.downs)}</p>
+                    <button  onMouseOver = {onMouseOverHandlerDown} onMouseOut = {onMouseOutHandlerDown} className ='down-vote'><img src='https://styles.redditmedia.com/t5_2qnty/styles/postDownvoteIconInactive_cnbj1c0wj6x11.png' alt ='down-vote'/></button>
+            </div>
+            <div className='post-container'>
+                <h1 className='post-title'>{post.title}</h1>
+                <p className = 'post-text'>{post.selftext}</p>
+                <div className = 'video-container'>
+                    <video width="320px" length = "480px" controls autoplay>
+                        <source src={post.media.reddit_video.scrubber_media_url} type="video/mp4"/>
+                        <audio src={post.media.reddit_video.hls_url} type = "audio/mpeg"/>
+                    </video>
+                </div>
+            </div>
+        </div>  
+        )
+        }
+       else{
             return(
     <div className = 'post-card-container'>
         <div className= "vote-container">
@@ -37,7 +78,9 @@ const onMouseOutHandlerDown = (e) =>{
             <p className = 'post-text'>{post.selftext}</p>
         </div>
     </div>            
-            ) 
+        ) 
+            }    
+              
           
         })
     }
