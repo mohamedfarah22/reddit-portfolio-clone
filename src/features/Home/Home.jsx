9b25877d0b0291f13cdc905d46dccc96ff442af0
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-20 00:04:32 +1100

    feat: added filtered func selector function and filtered post by search term inserted in header

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -4,8 +4,13 @@ import { selectPosts } from "../../store/redditSlice";
 import {loadPosts} from '../../store/redditSlice';
 import { Posts } from "../../Components/postCards/Posts";
 import { selectCurrentSubreddit } from "../../store/redditSlice";
+import { selectFilteredPosts } from "../../store/redditSlice";
+import { selectSearchTerm } from "../../store/redditSlice";
 export function Home(){
-    const posts = useSelector(selectPosts);
+    const searchTerm = useSelector(selectSearchTerm)
+    const filterFunc = (post) => post.title.toLowerCase().includes(searchTerm)
+    const posts = useSelector(selectPosts)
+    const filteredPosts = useSelector(selectFilteredPosts(filterFunc))
     const currentSubreddit= useSelector(selectCurrentSubreddit)
     const dispatch = useDispatch();
     
@@ -13,6 +18,7 @@ export function Home(){
         dispatch(loadPosts(currentSubreddit));
     }, [dispatch, currentSubreddit])
 return(
-    <Posts  posts = {posts}/>
+   <Posts posts = {filteredPosts} />
+
 )
 }
