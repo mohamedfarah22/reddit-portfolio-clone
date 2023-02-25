bf12c6faff241954cba218843dbef7254a6c4d05
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-02-25 18:54:17 +1100

    enclosed header in link tag to link back to home page when clicked

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -3,10 +3,10 @@ import logo from '../../logo.png'
 import loginLogo from'../../login-logo.jpeg'
 import React, {useState, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
-
+import { Link } from 'react-router-dom';
 
 import {setSearchPhrase} from "../../store/redditSlice"
-export function Header(props){
+export function Header(){
 const [searchTerm, setSearchTerm] = useState('');
 const dispatch  = useDispatch()
 //submit function needs to be completed.
@@ -18,13 +18,17 @@ const handleChange = (e) =>{
     setSearchTerm(e.target.value)  
 }
 
+
 useEffect(() =>{
 dispatch(setSearchPhrase(searchTerm))
 }, [dispatch, searchTerm])
 
+
     return(
         <div className="header-containter">
-            <img className="reddit-logo" src={logo}alt="logo of reddit"/>
+            <Link to='/'>
+            <img className="reddit-logo" src={logo}alt="logo of reddit" />
+            </Link>
             <form className="searchBar" onSubmit = {handleSubmit}>
                 <input type="text" value= {searchTerm} onChange = {handleChange}/>
                 <button type="submit"><i class="fa fa-search"></i>Submit</button>
