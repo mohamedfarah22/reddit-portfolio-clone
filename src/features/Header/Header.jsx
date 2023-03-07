3285e4d79b10e31b169f719a12e01fc53fb861e3
mohamedfarah22 <73460475+mohamedfarah22@users.noreply.github.com>
2023-03-07 19:22:16 +1100

    feat: added on submit search feature to show post search results in subreddit

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -3,19 +3,24 @@ import logo from '../../logo.png'
 import loginLogo from'../../login-logo.jpeg'
 import React, {useState, useEffect} from 'react';
 import {useDispatch} from 'react-redux';
-import { Link } from 'react-router-dom';
-
+import { Link, useHistory} from 'react-router-dom';
 import {setSearchPhrase} from "../../store/redditSlice"
+
 export function Header(){
 const [searchTerm, setSearchTerm] = useState('');
 const dispatch  = useDispatch()
 //submit function needs to be completed.
+const history = useHistory()
 const handleSubmit = (e) =>{
     e.preventDefault();
 
 }
 const handleChange = (e) =>{
     setSearchTerm(e.target.value)  
+}
+const onClickHandler = () =>{
+    history.push('/')
+
 }
 
 
@@ -31,7 +36,8 @@ dispatch(setSearchPhrase(searchTerm))
             </Link>
             <form className="searchBar" onSubmit = {handleSubmit}>
                 <input type="text" value= {searchTerm} onChange = {handleChange}/>
-                <button type="submit"><i class="fa fa-search"></i>Submit</button>
+                <button className='search-button' type="submit" onClick = {onClickHandler}><i class="fa fa-search"></i>Submit</button>
+            
             </form>
             <img className="login-logo" src={loginLogo}alt="login logo of reddit"/>
          </div>
